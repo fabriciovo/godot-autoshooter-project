ba89ce40dbc8cca1f0b6b8c40e337983ced7a983
fabricio <fabricio.varisco@outlook.com>
2025-03-08 20:12:58 -0300

    Update base_shoot.gd

--- Scripts/base_shoot.gd
+++ Scripts/base_shoot.gd
@@ -10,5 +10,4 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	if _body.is_in_group("Monster"):
-		queue_free()
+	queue_free()
